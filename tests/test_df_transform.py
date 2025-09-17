fa7f1e3e695c3e347b38bf895b39164a44879a01
Tsai, Cheng Lun <cheng.lun.tsai@intel.com>
2025-09-17 15:05:54 +0800

    4th commit

--- tests/test_df_transform.py
+++ tests/test_df_transform.py
@@ -8,9 +8,9 @@ test_path = Path(__file__).parent
 golden_input = test_path / "golden_input" / "golden_input.xlsx"
 
 CASES = [
-    ("add_new_column", df_transform.add_new_column, test_path / "golden_output" / "df_new_col.xlsx"),
-    ("reverse_columns", df_transform.reverse_columns, test_path / "golden_output" / "df_reversed.xlsx"),
-]
+    ("add_new_column", df_transform.add_new_column, test_path / "golden_output" / "add_new_column.xlsx"),
+    ("reverse_columns", df_transform.reverse_columns, test_path / "golden_output" / "reverse_columns.xlsx"),
+] # define the test, the order is test_name / the function want to test / golden output path
 
 @pytest.mark.parametrize("name,function,output_excel", CASES, ids=[case[0] for case in CASES])
 def test_df(name, function, output_excel):
@@ -18,3 +18,5 @@ def test_df(name, function, output_excel):
     golden_output = function(df_golden_input)
     output_df = pd.read_excel(output_excel)
     assert_frame_equal(golden_output, output_df, check_dtype=True, check_exact=True)
+
+    
